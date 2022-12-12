a6bc774bb9fe6d7d063483fd81555430053214d4
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2022-12-12 18:40:17 -0500

    adding middle ware and html routes

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+var syncOptions = { force: true };
+
 var app = express();
 var PORT = process.env.PORT || 3001;
 
