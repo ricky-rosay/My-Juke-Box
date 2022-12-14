48c52956f0765860eda86d08ce4cbee38d9291d6
imran-rassi <imran.rassi.2020@gmail.com>
2022-12-13 22:02:29 -0500

    added the latest updates

--- models/rating.js
+++ models/rating.js
@@ -1,46 +1,45 @@
 module.exports = function(sequelize, DataTypes) {
-    var Rating = sequelize.define("Rating", {
-      value: {
-        type: DataTypes.BOOLEAN
-      },
-      songName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      genre: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      artist: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
+  var Rating = sequelize.define("Rating", {
+    value: {
+      type: DataTypes.BOOLEAN
+    },
+    songName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    artist: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  Rating.associate = function(models) {
+    Rating.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
       }
     });
-  
-    Rating.associate = function(models) {
-      Rating.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-  
-    Rating.associate = function(models) {
-      Rating.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-    return Rating;
   };
-  
+
+  Rating.associate = function(models) {
+    Rating.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  return Rating;
+};
